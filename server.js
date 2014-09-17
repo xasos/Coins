c7398be31b5b6fa1bae7b1d535a57dfaf9396612
Niraj Pant <nirajpant7@gmail.com>
2014-09-16 20:18:27 -0500

    add server

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+var router = express.Router();
+
+router.get('/', function(req, res) {
+	res.json({ message: 'Welcome to coins-api!' });
+});
+
+app.use('/', router);
+
+app.listen(port);
+console.log('Listening on port: ' + port);
