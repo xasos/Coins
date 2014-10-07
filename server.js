54489d6432387638a9d86a5692e2cb0d2cb766b6
Niraj Pant <nirajpant7@gmail.com>
2014-10-06 22:44:00 -0500

    update dependecies

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var cheerio = require('cheerio');
+var request = require('request');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
