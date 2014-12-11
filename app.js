195a188c4adffb2271edbdda4c4eee2cb94adb1a
Niraj Pant <nirajpant7@gmail.com>
2014-12-11 13:18:25 -0600

    Update app.js

--- app.js
+++ app.js
@@ -21,12 +21,7 @@ router.get('/', function(req, res) {
 	res.json({ message: 'Welcome to coins-api! Documentation for querying the API is available here: https://github.com/xasos/Coins.' });
 });
 
-router.get('/coins/:id', function (req, res, next) {
-  console.log('asdasd');
-  next();
-});
-
-router.route('/coins') 
+router.route('/coins/:id') 
 	.get(function(req, res) {		
 		request(target, function(err, resp, body) {
 		if (!err && resp.statusCode == 200) {
