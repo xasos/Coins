684fa27d03faf39085a503b4b2b271535454d3a6
Niraj Pant <nirajpant7@gmail.com>
2014-12-11 13:34:20 -0600

    Update app.js

--- app.js
+++ app.js
@@ -61,56 +61,6 @@ router.route('/coins/:id?')
 		res.json(coinList);
 	});
 
-// var scrapeCoins = function(numCoins) {
-// 	request(target, function(err, resp, body) {
-// 		if (!err && resp.statusCode == 200) {
-// 			var $ = cheerio.load(body);
-
-// 			$('tr').each(function(i) {
-// 				if ($(this).attr("id")) {
-// 					var coinName = $(this).attr("id").slice(3);
-// 				}
-
-// 				var pos = $(this).find('td').eq(0).text().trim();
-
-// 				var marketCap = $(this).find('td').eq(2).text().trim();
-// 				marketCap = marketCap.slice(2).replace(/,/g, "");
-				
-// 				var price = $(this).find('td').eq(3).text().trim();
-// 				price = price.slice(2).replace(/,/g, "");
-
-// 				var ticker = $(this).find('td').eq(4).text().trim();
-// 				ticker = ticker.split('\n').slice(1, 2).join('\n').trim();
-
-// 				var volume = $(this).find('td').eq(5).text().trim();
-// 				volume = volume.slice(2).replace(/,/g, "");
-
-// 				var delta24hr = $(this).find('td').eq(6).text().trim();
-// 				delta24hr = delta24hr.slice(0, -2);
-
-// 				var coins = {'name': coinName, 'position': pos, 'price': price, 'ticker': ticker, 'volume': volume, 'delta24hr': delta24hr, 'timestamp': Date.now()};
-// 				if(coinName) {
-// 					coinList.push(coins);
-// 				}				
-// 				console.log(coinList);
-// 			});					
-// 		}
-// 	});	
-// };
-
-// router.route('/coins/:coin_id') 
-// 	.get(function(req, res) {
-// 		Coin.findById(req.params.coin_id, function(err, coin) {
-// 			if (err) {
-// 				res.send(err);
-// 			};
-
-// 			res.json(coin);
-// 		});
-// 	});
-// }
-
 app.use('/', router);
-
 app.listen(port);
 console.log('Listening on port ' + port);
